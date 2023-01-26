d980d5edfafec3e419a57374426cee62e8dbd644
jng9230 <jng9230@gmail.com>
2023-01-25 19:52:39 -0500

    remove example files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 /*
 topmost file that calls everything else
 */
-import {init_example} from "./example/example";
 import {Maze} from "./maze";
 import "./index.css";
 
-init_example();
 window.onload = () => {
     const maze = new Maze(10,15,50);
     maze.highlight_path();
